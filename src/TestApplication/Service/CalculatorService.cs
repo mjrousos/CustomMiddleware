80f2895209c0e6c6fb8caa7625b4a3ec5a187993
Mike Rousos <MikeRou@Microsoft.com>
2016-10-26 09:04:47 -0700

    Add custom middleware sample, demonstrating what's discussed in my [custom soap middleware blog](https://blogs.msdn.microsoft.com/dotnet/2016/09/19/custom-asp-net-core-middleware-example/)

---
+++ src/TestApplication/Service/CalculatorService.cs
@@ -0,0 +1,25 @@
+﻿using System.ServiceModel;
+
+namespace TestApplication
+{
+    public class CalculatorService : ICalculatorService
+    {
+        public double Add(double x, double y) => x + y;
+        public double Divide(double x, double y) => x / y;
+        public double Multiply(double x, double y) => x * y;
+        public double Subtract(double x, double y) => x - y;
+    }
+
+    [ServiceContract]
+    public interface ICalculatorService
+    {
+        [OperationContract]
+        double Add(double x, double y);
+        [OperationContract]
+        double Subtract(double x, double y);
+        [OperationContract]
+        double Multiply(double x, double y);
+        [OperationContract]
+        double Divide(double x, double y);
+    }
+}
---
+++ src/TestClient/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+
+namespace TestClient
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Please provide the remote URL of the calculator service");
+                return;
+            }
+
+            // Create random inputs
+            Random numGen = new Random();
+            double x = numGen.NextDouble() * 20;
+            double y = numGen.NextDouble() * 20;
+
+            var serviceAddress = $"{args[0]}/CalculatorService.svc";
+
+            var client = new CalculatorServiceClient(new BasicHttpBinding(), new EndpointAddress(serviceAddress));
+            Console.WriteLine($"{x} + {y} == {client.Add(x, y)}");
+            Console.WriteLine($"{x} - {y} == {client.Subtract(x, y)}");
+            Console.WriteLine($"{x} * {y} == {client.Multiply(x, y)}");
+            Console.WriteLine($"{x} / {y} == {client.Divide(x, y)}");
+        }
+    }
+    class CalculatorServiceClient : ClientBase<ICalculatorService>
+    {
+        public CalculatorServiceClient(Binding binding, EndpointAddress remoteAddress) : base(binding, remoteAddress) { }
+        public double Add(double x, double y) => Channel.Add(x, y);
+        public double Subtract(double x, double y) => Channel.Subtract(x, y);
+        public double Multiply(double x, double y) => Channel.Multiply(x, y);
+        public double Divide(double x, double y) => Channel.Divide(x, y);
+    }
+
+    [ServiceContract]
+    public interface ICalculatorService
+    {
+        [OperationContract]
+        double Add(double x, double y);
+        [OperationContract]
+        double Subtract(double x, double y);
+        [OperationContract]
+        double Multiply(double x, double y);
+        [OperationContract]
+        double Divide(double x, double y);
+    }
+}
